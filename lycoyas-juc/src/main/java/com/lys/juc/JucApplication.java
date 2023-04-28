bdcc9d23991c30c1ab85d562f5377bd6251f860e
Lycoyas <1341648608@qq.com>
2023-04-28 18:47:47 +0800

    feat:JUC init date:2023-04-28

---
+++ lycoyas-juc/src/main/java/com/lys/juc/JucApplication.java
@@ -0,0 +1,18 @@
+package com.lys.juc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lycoyas
+ * @create 2023-04-28 18:45
+ * @description
+ */
+@SpringBootApplication
+public class JucApplication {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JucApplication.class, args);
+    }
+
+}
